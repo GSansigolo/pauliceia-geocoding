71d33a792a6f658da55ac423e6b8952346842821
Gabriel Sansigolo <gabrielsansigolo@gmail.com>
2017-07-11 16:04:15 -0300

    Dia 11-07

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,8 @@
 	var router = express.Router();
   var GeoJSON = require('geojson');
   var postgeo = require("postgeo");
+  var js2xmlparser = require("js2xmlparser");
+
 
 /*  
 +---------------------------------------------------+
@@ -27,11 +29,12 @@
 +---------------------------------------------------+*/
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Pauliceia 2.0 WebService' });
+
 });
 
 /*  
 +---------------------------------------------------+
-|getAllStreets-GEOJSON
+|getAllStreets-GeoJson
 +---------------------------------------------------+*/
 
 router.get('/getAllStreets/geojson', (req, res, next) => {
@@ -66,7 +69,7 @@ const results = [];
 
 /*  
 +---------------------------------------------------+
-|getAllStreets-JSON
+|getAllStreets-Json
 +---------------------------------------------------+*/
 
 router.get('/getAllStreets/json', (req, res, next) => {
@@ -96,10 +99,44 @@ const results = [];
   });
 });
 
+/*  
++---------------------------------------------------+
+|getAllStreets-Xml
++---------------------------------------------------+*/
+
+router.get('/getAllStreets/xml', (req, res, next) => {
+const results = [];
+
+  // Get a Postgres client from the connection pool
+  pg.connect(connectionString, (err, client, done) => {
+    // Handle connection errors
+    if(err) {
+      done();
+      console.log(err);
+      return res.status(500).json({success: false, data: err});
+    }
+    // SQL Query > Select Data
+    const query = client.query('select * from tb_street order by id ASC;');
+    
+    // Stream results back one row at a time
+    query.on('row', (row) => {
+      results.push(row);
+    });
+    // After all data is returned, close connection and return results
+    query.on('end', () => {
+      done();
+      
+      const results2 = js2xmlparser.parse("tb_street", results);
+      //console.log(results2);
+
+      return res.write(results2);;
+    });
+  });
+});
 
 /*  
 +---------------------------------------------------+
-|getSingleStreet-JSON
+|getSingleStreet-Json
 +---------------------------------------------------+*/
 router.get('/getSingleStreet/json/:street_id', (req, res, next) => {
   
@@ -136,7 +173,7 @@ router.get('/getSingleStreet/json/:street_id', (req, res, next) => {
 
 /*  
 +---------------------------------------------------+
-|getSingleStreet-GEOJSON
+|getSingleStreet-GeoJson
 +---------------------------------------------------+*/
 router.get('/getSingleStreet/geojson/:street_id', (req, res, next) => {
   
@@ -170,6 +207,44 @@ router.get('/getSingleStreet/geojson/:street_id', (req, res, next) => {
     });
   });
 });
+
+/*  
++---------------------------------------------------+
+|getSingleStreet-Xml
++---------------------------------------------------+*/
+router.get('/getSingleStreet/xml/:street_id', (req, res, next) => {
+  
+  const results = [];
+  const id = req.params.street_id;
+
+  // Get a Postgres client from the connection pool
+  pg.connect(connectionString, (err, client, done) => {
+    // Handle connection errors
+    if(err) {
+      done();
+      console.log(err);
+      return res.status(500).json({success: false, data: err});
+    }
+    // SQL Query > Select Data
+    const query = client.query('select * from tb_street where id=($1)',[id]);
+    //const query = client.query('select * from tb_street where id=($1)', id);
+
+// Stream results back one row at a time
+    query.on('row', (row) => {
+      results.push(row);
+    });
+    // After all data is returned, close connection and return results
+    query.on('end', () => {
+      done();
+
+      const results2 = js2xmlparser.parse("tb_street", results);
+      //console.log(results2);
+
+      return res.write(results2);;
+    });
+  });
+});
+
 
 /*  
 +---------------------------------------------------+
