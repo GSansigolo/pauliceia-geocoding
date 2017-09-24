b925d1a78c22d3f3a890c7713a1c92a366abde01
Gabriel Sansigolo <gabrielsansigolo@gmail.com>
2017-09-24 11:27:20 -0300

    Dia 24-09

--- views/html/map-page/js/onload.js
+++ views/html/map-page/js/onload.js
@@ -1,20 +1,24 @@
 function onload(){
-
     var url = (localStorage.getItem("storageURL"));
     alert(url);
-    (function () {
-      var myData;
-
-      function test(callback) {
-        $.getJSON(url, function (data) {
-          callback(data);
-        });
-      }
-
-      test(function (data) {
-        geom = data.geom;
-      });
+    loadJSON(function(response) {
+        // Parse JSON string into object
+        var actual_JSON = JSON.parse(response);
     });
+    alert(actual_JSON);
+}
 
-    alert(geom);
+function loadJSON(callback) {
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', url, true);
+    // Replace 'my_data' with the path to your file
+    xobj.onreadystatechange = function() {
+        if (xobj.readyState === 4 && xobj.status === "200") {
+            // Required use of an anonymous callback 
+            // as .open() will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+        }
+    };
+    xobj.send(null);
 }
