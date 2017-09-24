a653c66a0edcc26403d046f362c602b15e91f397
Gabriel Sansigolo <gabrielsansigolo@gmail.com>
2017-09-24 10:10:53 -0300

    Dia 24-09

--- views/html/map-page/js/map.js
+++ views/html/map-page/js/map.js
@@ -1,4 +1,16 @@
 
 function onload(){
-    //(localStorage.getItem("storageURL"))
+    var url = (localStorage.getItem("storageURL"));
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+           // Typical action to be performed when the document is ready:
+           document.getElementById("geom").innerHTML = xhttp.responseText;
+        }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+
+    alert(xhttp);
 }
