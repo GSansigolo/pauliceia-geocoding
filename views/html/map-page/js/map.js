1b7cffb96219d71f1190674fa4942527a656fb54
Gabriel Sansigolo <gabrielsansigolo@gmail.com>
2017-09-24 10:41:41 -0300

    Dia 24-09

--- views/html/map-page/js/map.js
+++ views/html/map-page/js/map.js
@@ -17,28 +17,5 @@ function onload(){
     request.open('GET', url, true);
     request.send();
 
-    request.onreadystatechange = function(response) {
-        if (request.readyState === 4) {
-          if (request.status === 200) {
-            // Parse the JSON
-            var jsonOptions = JSON.parse(request.responseText);
-        
-            // Loop over the JSON array.
-            jsonOptions.forEach(function(item) {
-              // Create a new <option> element.
-              var option = document.createElement('option');
-              // Set the value using the item in the JSON array.
-              option.value = item;
-              // Add the <option> element to the <datalist>.
-              dataList.appendChild(option);
-            });
-            
-            // Update the placeholder text.
-            input.placeholder = "";
-          } else {
-            // An error occured :(
-            }
-        }
-    };
-    alert(document.getElementById('option'));
+    alert();
 }
