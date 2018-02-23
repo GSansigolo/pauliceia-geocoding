b8452d197c4cacf3a9f08fd7b76310fbf8950ef0
Gabriel Sansigolo <gabrielsansigolo@gmail.com>
2018-02-23 17:56:58 -0300

    Dia 23-02

--- models/database.js
+++ models/database.js
@@ -1,9 +1,23 @@
 const pg = require('pg');
-const connectionString = process.env.DATABASE_URL || 'postgres://postgres:teste@localhost/db_pauliceia';
+
+const db_user = process.env.DATABASE_USER || "postgres";
+const db_pass = process.env.DATABASE_PASS || "teste";
+const db_host = process.env.DATABASE_HOST || "localhost";
+const db_name = process.env.DATABASE_NAME || "db_pauliceia";
+
+//console.log("-user: "+db_user+" -pass: "+db_pass+" -host: "+db_host+" -name: "+db_name);
+
+const connectionString = {
+  host: db_host,
+  port: 5432,
+  user: db_user,
+  database: db_name,
+  password: db_pass
+}
 
 const client = new pg.Client(connectionString);
-client.connect();
-const query = client.query('select * from tb_street');
+
+const query = connectionString.query('select * from tb_street');
 console.log('Done!')
-query.on('end', () => { client.end(); });
+query.on('end', () => { connectionString.end(); });
 
--- routes/index.js
+++ routes/index.js
@@ -23,12 +23,19 @@
 +--------------------------------------------------*/
   const pg = require('pg');
 
-  const db_user = "postgres";
-  const db_pass = "teste";
-  const db_adre = "localhost";
-  const db_name = "db_pauliceia";
+  const db_user = process.env.DATABASE_USER || "postgres";
+  const db_pass = process.env.DATABASE_PASS || "teste";
+  const db_host = process.env.DATABASE_HOST || "localhost";
+  const db_name = process.env.DATABASE_NAME || "db_pauliceia";
 
-  const connectionString = process.env.DATABASE_URL || 'postgres://'+db_user+':'+db_pass+'@'+db_adre+'/'+db_name;
+  const connectionString = {
+    host: db_host,
+    port: 5432,
+    user: db_user,
+    database: db_name,
+    password: db_pass
+  }
+
   const client = new pg.Client(connectionString);
   
   client.connect();
