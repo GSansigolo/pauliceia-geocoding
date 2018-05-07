88433fa61ab926066196d6c5a7bc472a6fbcd3cb
Gabriel Sansigolo <gabrielsansigolo@gmail.com>
2018-05-07 13:03:44 -0300

    Dia 07-05

--- controllers/closestPoint.js
+++ controllers/closestPoint.js
@@ -1,6 +1,19 @@
 //line -> geometria do rua
 //point-> geometria do ponto
 
-exports.closestPoint = function(line, point){
+//exports.closestPoint = function(line, point){
 
+function closestPoint(line, point){
+
+    //tratar a string da geometria linha
+    var geomLine = line.substr(line.indexOf("(")+2);
+    geomLine = geomLine.substr(0,geomLine.indexOf(")"));
+
+    //tratar a string da geometria linha
+    var geomPoint = point.substr(point.indexOf("(")+2);
+    geomPoint = geomPoint.substr(0,geomPoint.indexOf(")"));
+
+    alert("geomPoint: " + geomPoint + "\n \n" + "geomLine: " + geomLine);
+
+ 	//return geomPoint, geomLine;
 }
