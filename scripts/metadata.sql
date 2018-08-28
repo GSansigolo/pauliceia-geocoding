930d39cec8177ee92fe5c51f53ad275454b9319d
Gabriel Sansigolo <gabrielsansigolo@gmail.com>
2018-08-28 16:59:18 -0300

    Metadata

--- scripts/metadata.sql
+++ scripts/metadata.sql
@@ -5,18 +5,18 @@
     CREATE TABLE metadata (
         tname text, 
         created timestamptz, 
-        details json
+        column_details json
     );
 
 /* Insert into "metadata" holder table */
 
     INSERT INTO metadata 
-        SELECT 'tb_places', now(), '{"organization":"n/a","source":"manual","catalog":false}';
+        SELECT 'tb_places', now(), '{"name_column":"name", "number_column":"number", "firstYear_column":"first_year", "lastYear_column":"last_year", "geom_column":"geom"}';
 
 /* Insert into "metadata" holder table */
 
     INSERT INTO metadata 
-        SELECT 'tb_places2', now(), '{"organization":"n/a","source":"manual","catalog":false}';
+        SELECT 'tb_places2', now(), '{"name_column":"name", "number_column":"number", "firstYear_column":"first_year", "lastYear_column":"last_year", "geom_column":"geom"}';
 
 /* View the "metadata" holder table */
 
